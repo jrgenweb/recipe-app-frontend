fdfc27b226177931b63170f6b906f7e31f97d0ee
Nemeth Laszlo <nemeth.rzr@gmail.com>
2026-02-11 13:09:10 +0100

    fix(nav-bar) dashboard menu

--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -1,13 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../shared/services/auth-service';
-import { AsyncPipe } from '@angular/common';
 
 import { ProfilePicture } from '../profile-picture/profile-picture';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink, AsyncPipe, RouterLinkActive, ProfilePicture],
+  imports: [RouterLink, RouterLinkActive, ProfilePicture],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss',
 })
