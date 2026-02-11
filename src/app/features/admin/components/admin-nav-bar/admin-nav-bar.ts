ad9ffe46694079b3b2ceaa311e9769e15aef5fe9
Nemeth Laszlo <nemeth.rzr@gmail.com>
2026-02-11 13:05:32 +0100

    refactor(admin-nav-bar)

--- src/app/features/admin/components/admin-nav-bar/admin-nav-bar.ts
+++ src/app/features/admin/components/admin-nav-bar/admin-nav-bar.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../../shared/services/auth-service';
+import { ProfilePicture } from '../../../../components/profile-picture/profile-picture';
 
 @Component({
   selector: 'app-admin-nav-bar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ProfilePicture],
   templateUrl: './admin-nav-bar.html',
   styleUrl: './admin-nav-bar.scss',
 })
-export class AdminNavBar {}
+export class AdminNavBar {
+  public auth = inject(AuthService);
+
+  onLogout(event: Event) {
+    event.preventDefault();
+    this.auth.logout();
+  }
+}
