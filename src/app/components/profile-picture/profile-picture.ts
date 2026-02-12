f6d0f92a234ddc0811037eb591bf6eeb89ff048d
Nemeth Laszlo <nemeth.rzr@gmail.com>
2026-02-12 11:43:12 +0100

    added default pictures

--- src/app/components/profile-picture/profile-picture.ts
+++ src/app/components/profile-picture/profile-picture.ts
@@ -8,13 +8,13 @@ import { onImageError } from '../../shared/functions';
   styleUrl: './profile-picture.scss',
 })
 export class ProfilePicture {
-  @Input() src!: string;
+  @Input() src?: string;
   @Input() title = '';
   @Input() size: 'md' | 'sm' | 'lg' = 'md';
 
   constructor() {}
 
   onImageError(event: Event) {
-    onImageError(event);
+    onImageError(event, '/no-user.avif');
   }
 }
