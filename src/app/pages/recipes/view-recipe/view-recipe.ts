367f6201f4f4d599d8bbed47f7f5e745655f44bf
Laszlo Nemeth <nemeth.rzr@gmail.com>
2026-03-15 14:13:28 +0100

    add spinner to recipe-view

--- src/app/pages/recipes/view-recipe/view-recipe.ts
+++ src/app/pages/recipes/view-recipe/view-recipe.ts
@@ -1,8 +1,8 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
-import { catchError, EMPTY, map } from 'rxjs';
+import { map } from 'rxjs';
 import { DatePipe } from '@angular/common';
 
 import { TimeAgoPipe } from '../../../shared/pipes/time-ago-pipe';
@@ -10,24 +10,32 @@ import { TimeAgoPipe } from '../../../shared/pipes/time-ago-pipe';
 import { ProfilePicture } from '../../../components/profile-picture/profile-picture';
 import { Rating } from '../../../components/rating/rating';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { IRecipeDetail } from '@recipe/shared';
+
 import { IRecipeCommentResponse } from '@recipe/shared';
 import { RecipeGallery } from '../../../features/recipes/components/recipe-gallery/recipe-gallery';
 import { AddComment } from '../../../features/recipes/components/add-comment/add-comment';
-import { RecipeService } from '../../../features/recipes/services/recipe-service';
+
 import { RecipeStore } from '../../../features/recipes/stores/recipe.store';
 import { CommentStore } from '../../../features/comments/store/comment.store';
+import { Spinner } from '../../../components/spinner/spinner';
 
 @Component({
   selector: 'app-view-recipe',
-  imports: [RouterLink, TimeAgoPipe, RecipeGallery, ProfilePicture, DatePipe, Rating, AddComment],
+  imports: [
+    RouterLink,
+    TimeAgoPipe,
+    RecipeGallery,
+    ProfilePicture,
+    DatePipe,
+    Rating,
+    AddComment,
+    Spinner,
+  ],
   templateUrl: './view-recipe.html',
   styleUrl: './view-recipe.scss',
 })
 export class ViewRecipe implements OnInit {
-  private recipeService = inject(RecipeService);
   private route = inject(ActivatedRoute);
-  private router = inject(Router);
 
   public recipeStore = inject(RecipeStore);
   public commentStore = inject(CommentStore);
@@ -40,59 +48,19 @@ export class ViewRecipe implements OnInit {
   //  recipe = signal<IRecipeDetail | undefined>(undefined);
   comments = signal<IRecipeCommentResponse[]>([]);
 
-  //avgRating = signal(this.recipe()?.avgRating || 0); // kezdeti érték a recept értékelése, vagy 0, ha nincs
-
   ngOnInit() {
     this.recipeStore.loadDetail(this.recipeId() || ''); // IDEIGLENESEN
     this.commentStore.loadAll(this.recipeId() || ''); // IDEIGLENESEN
   }
-  /*
-  loadRecipe() {
-    this.recipeService
-      .getRecipeWithComments(this.recipeId() || '')
-      .pipe(
-        catchError(() => {
-          this.router.navigate(['/error']);
-          return EMPTY;
-        }),
-        //tap((res) => {}),
-      )
-      .subscribe((res) => {
-        this.recipe.set(res.recipe);
-        this.comments.set(res.comments);
-        this.avgRating.set(res.recipe.avgRating);
-      });
-  }
-*/
-  //Rating visszatöltése a backendről, pl. új értékelés után
+
   loadRating() {}
-
-  // Computed view model
-  /*
-  vm = computed(() => {
-    const recipe = this.recipe();
-
-    return {
-      recipe: this.recipe(),
-      hasRecipe: !!recipe,
-      comments: this.comments() ?? [],
-    };
-  });*/
 
   constructor() {}
 
   // Új komment esetén frissítés
-  onComment(_state: boolean) {
-    // újra lekéri a receptet
-    //this.loadRecipe();
-    //this.loadComments(); majd később
-  }
+  onComment(_state: boolean) {}
 
   onChangeRate(rate: { recipeId: string; rate: number }) {
     this.recipeStore.updateRating(rate.recipeId, rate.rate);
-    //this.recipeService.updateRating(rate.recipeId, rate.rate);
-    //this.avgRating.set(rate.rate); // frissíti a jelzett értékelést
-
-    //this.loadRecipe();
   }
 }
